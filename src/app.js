ea73c3f5434ad6c47b0cf3186927b6a1256eb751
Nicolas Tripodi <tripo@Nicolass-Mini.fibertel.com.ar>
2023-04-07 20:44:45 -0300

    added products

--- src/app.js
+++ src/app.js
@@ -7,6 +7,16 @@ const port = 8080;
 
 const producto = new ProductManager('../archivo.json');
 
+// const add = async () => {
+//     await producto.addProduct({title: 'manzana', description:'deliciosa', price:5, thumbnail:'manzana.png', code: "man1", stock:600 });
+//     await producto.addProduct({title: 'modem', description:'technicolor', price:500, thumbnail:'modem.png', code: "mod1", stock:40 });
+//     await producto.addProduct({title: 'botella', description:'negra', price:25, thumbnail:'botella.png', code: "bot1", stock:10 });
+//     await producto.addProduct({title: 'decodificador', description:'Flow', price:2500, thumbnail:'decoflow.png', code: "flw1", stock:6 });
+//     await producto.addProduct({title: 'iPad', description:'Air 10"', price:10000, thumbnail:'ipad.png', code: "ipd1", stock:1 });
+// }
+
+// add();
+
 // /products?limit=1
 server.get('/products', async (req, res) => {
     producto.load();
